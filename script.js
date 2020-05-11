93a17a7f4b7fb9837fbc383737175e89d24665f2
koebe1 <koe.be.pay@web.de>
2020-05-11 17:03:54 +0200

    added local storage saving to name and focus

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const time = document.getElementById('time'),
     go = document.getElementById('go'),
     mainContainer = document.getElementById('main-container'),
     start = document.getElementById('start'),
-    h1 = document.getElementById('h1');
+    h1 = document.getElementById('h1'),
+    errorMessage = document.getElementById('errorMessage');
 
 // Show Time
-const  showTime = () => {
+const showTime = () => {
     let today = new Date(),
         hour = today.getHours(),
         min = today.getMinutes(),
@@ -25,7 +26,7 @@ const  showTime = () => {
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
     // Show every second
-    setTimeout(showTime,1000);
+    setTimeout(showTime, 1000);
 }
 
 // Add Zero
@@ -38,7 +39,7 @@ const setBgGreet = () => {
     let today = new Date(),
         hour = today.getHours();
 
-    if(hour < 12) {
+    if (hour < 12) {
         // Morning
         document.body.style.backgroundImage = "url('../images/morning.jpg')";
         document.body.style.backgroundSize = "cover";
@@ -49,6 +50,7 @@ const setBgGreet = () => {
         document.body.style.backgroundImage = "url('../images/midday.jpg')";
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundPosition = "center";
+        document.body.style.color = "white";
         greeting.textContent = 'Good Afternoon';
     } else {
         // Evening
@@ -61,20 +63,40 @@ const setBgGreet = () => {
 }
 
 // Transition from start to main on button-click
-    go.addEventListener('click', () => {
-        // Get input value
-        const getName = document.getElementById('get-name').value;
-        const getFocus = document.getElementById('get-focus').value;
+go.addEventListener('click', () => {
+    // Get input value
+    const getName = document.getElementById('get-name').value;
+    const getFocus = document.getElementById('get-focus').value;
 
-        start.style.display = "none";
-        mainContainer.style.visibility = "visible";
+    // Show error message on empty fields
+    if (getName === "" || getFocus === "") {
+        const errorText = errorMessage.innerHTML = "Please fill out the fields!";
 
         // Get text from input to the main site
-        name.innerHTML = getName;
-        focus.innerHTML = getFocus;
-    });
+    } else {
+        start.style.display = "none";
+        mainContainer.style.display = "flex";
+        name.innerHTML = `, ${getName}!`;
+        focus.innerHTML = `${getFocus}!`;
 
+        // Save name and focus in local storage
+        localStorage.setItem('name', getName);
+        localStorage.setItem('focus', getFocus);
+    }
+});
+
+// Get local storage
+const getlocalStorage = () => {
+    name.innerHTML = localStorage.getItem('name');
+    focus.innerHTML = localStorage.getItem('focus');
+}
+
+// Initializing function
+const onInit = () => {
+    setBgGreet();
+    getlocalStorage();
+    showTime();
+}
 
 // Run
-showTime();
-setBgGreet();
+onInit();
